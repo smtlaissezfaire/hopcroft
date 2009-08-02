9ec371eb1143bcad2d1c9286e738ebd3f0142096
Scott Taylor <scott@railsnewbie.com>
2009-08-02 05:35:30 -0400

    Prepare for the bugfix: KleenStar should use subexpression matching

--- spec/hopcoft/regex/kleen_star_spec.rb
+++ spec/hopcoft/regex/kleen_star_spec.rb
@@ -4,38 +4,44 @@ module Hopcroft
   module Regex
     describe KleenStar do
       it "should take a regex" do
-        s = KleenStar.new("f")
-        s.expression.should == "f"
+        s = KleenStar.new(Char.new("f"))
+        s.expression.should == Char.new("f")
       end
 
       describe "matching" do
+        def new_kleen_star_with_string(str)
+          KleenStar.new(Char.new(str))
+        end
+
         it "should match 0 chars" do
-          s = KleenStar.new("a")
+          s = new_kleen_star_with_string("a")
           s.matches?("").should be_true
         end
 
         it "should match one char" do
-          s = KleenStar.new("a")
+          s = new_kleen_star_with_string("a")
           s.matches?("a").should be_true
         end
      
         it "should match a different char" do
-          s = KleenStar.new("a")
+          s = new_kleen_star_with_string("a")
           s.matches?("b").should be_true
         end
    
         it "should match many of the same chars" do
-          s = KleenStar.new("a")
-          s.matches?("aa").should be_true
+          pending 'FIXME: BUG' do
+            s = new_kleen_star_with_string("a")
+            s.matches?("aa").should be_true
+          end
         end
       end
    
       it "should have the regex string" do
-        KleenStar.new("a").to_regex_s.should == "a*"
+        KleenStar.new(Char.new("a")).to_regex_s.should == "a*"
       end
 
       it "should be able to output the state table" do
-        star = KleenStar.new("a")
+        star = KleenStar.new(Char.new("a"))
         
         lambda {
           star.to_machine.state_table.inspect
